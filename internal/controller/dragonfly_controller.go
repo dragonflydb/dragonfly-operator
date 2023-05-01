6dd40c75de79e085d278604892c208580f7ee5d0
Tarun Pothulapati <tarun@dragonflydb.io>
2023-05-01 17:18:18 +0530

    fix(rollout): Update rollout strategy to be `OnDelete` (#59)
    * fix(rollout): Update rollout strategy to be `OnDelete`
    
    Fixes https://github.com/dragonflydb/dragonfly-operator/issues/48
    
    This commit updates the statefulset's upgrade strategy to be `OnDelete`
    which means that the statefulset will not be automatically upgraded
    when the pod template changes. Instead, The Operator will delete
    each pod in the statefulset one at a time. This gives us the following
    benefits:
    - The order of pod rollout is controlled by the Operator which means
      we can start from replicas and go until master.
    - The Operator can wait for the pod to be ready and also be in stable
      sync before it moves to the next pod.
    - fix(resource): Remove `PhaseRollingUpdate` and use boolean

--- internal/controller/dragonfly_controller.go
+++ internal/controller/dragonfly_controller.go
@@ -19,6 +19,7 @@ package controller
 import (
 	"context"
 	"fmt"
+	"time"
 
 	dfv1alpha1 "github.com/dragonflydb/dragonfly-operator/api/v1alpha1"
 	"github.com/dragonflydb/dragonfly-operator/internal/resources"
@@ -88,7 +89,132 @@ func (r *DragonflyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 			return ctrl.Result{}, err
 		}
 
-		r.EventRecorder.Event(&df, corev1.EventTypeNormal, "Created", "Created resources for Dragonfly object")
+		r.EventRecorder.Event(&df, corev1.EventTypeNormal, "Resources", "Created resources")
+		return ctrl.Result{}, nil
+	} else if df.Status.IsRollingUpdate {
+		// This is a Rollout
+		log.Info("Rolling out new version")
+		var updatedStatefulset appsv1.StatefulSet
+		if err := r.Get(ctx, client.ObjectKey{Namespace: df.Namespace, Name: df.Name}, &updatedStatefulset); err != nil {
+			log.Error(err, "could not get statefulset")
+			return ctrl.Result{Requeue: true}, err
+		}
+
+		// get pods of the statefulset
+		var pods corev1.PodList
+		if err := r.List(ctx, &pods, client.InNamespace(df.Namespace), client.MatchingLabels(map[string]string{
+			"app":                               df.Name,
+			resources.KubernetesAppNameLabelKey: "dragonfly",
+		})); err != nil {
+			log.Error(err, "could not list pods")
+			return ctrl.Result{Requeue: true}, err
+		}
+
+		if len(pods.Items) != int(*updatedStatefulset.Spec.Replicas) {
+			log.Info("Waiting for all replicas to be ready")
+			return ctrl.Result{Requeue: true}, nil
+		}
+
+		// filter replicas to master and replicas
+		var master corev1.Pod
+		replicas := make([]corev1.Pod, 0)
+		for _, pod := range pods.Items {
+			if _, ok := pod.Labels[resources.Role]; ok {
+				if pod.Labels[resources.Role] == resources.Replica {
+					replicas = append(replicas, pod)
+				} else if pod.Labels[resources.Role] == resources.Master {
+					master = pod
+				}
+			} else {
+				log.Info("found pod without label", "pod", pod.Name)
+				// retry after they are ready
+				return ctrl.Result{RequeueAfter: 5 * time.Second}, nil
+			}
+		}
+
+		// We want to update the replicas first then the master
+		// We want to have at most one updated replica in full sync phase at a time
+		// if not, requeue
+		fullSyncedUpdatedReplicas := 0
+		for _, replica := range replicas {
+			// Check only with latest replicas
+			onLatestVersion, err := isPodOnLatestVersion(ctx, r.Client, &replica, &updatedStatefulset)
+			if err != nil {
+				log.Error(err, "could not check if pod is on latest version")
+				return ctrl.Result{RequeueAfter: 5 * time.Second}, err
+			}
+			if onLatestVersion {
+				// check if the replica had a full sync
+				log.Info("New Replica found. Checking if replica had a full sync", "pod", replica.Name)
+				isStableState, err := isStableState(ctx, r.Client, &replica)
+				if err != nil {
+					log.Error(err, "could not check if pod is in stable state")
+					return ctrl.Result{RequeueAfter: 5 * time.Second}, err
+				}
+
+				if !isStableState {
+					log.Info("Not all new replicas are in stable status yet", "pod", replica.Name, "reason", err)
+					return ctrl.Result{RequeueAfter: 5 * time.Second}, err
+				}
+				log.Info("Replica is in stable state", "pod", replica.Name)
+				fullSyncedUpdatedReplicas++
+			}
+		}
+
+		log.Info(fmt.Sprintf("%d/%d are in stable state", fullSyncedUpdatedReplicas, len(replicas)))
+
+		// if we are here it means that all latest replicas are in stable sync
+		// delete older version replicas
+		for _, replica := range replicas {
+			// Check if pod is on latest version
+			onLatestVersion, err := isPodOnLatestVersion(ctx, r.Client, &replica, &updatedStatefulset)
+			if err != nil {
+				log.Error(err, "could not check if pod is on latest version")
+				return ctrl.Result{RequeueAfter: 5 * time.Second}, err
+			}
+
+			if !onLatestVersion {
+				// delete the replica
+				log.Info("deleting replica", "pod", replica.Name)
+				r.EventRecorder.Event(&df, corev1.EventTypeNormal, "Rollout", "Deleting replica")
+				if err := r.Delete(ctx, &replica); err != nil {
+					log.Error(err, "could not delete pod")
+					return ctrl.Result{RequeueAfter: 5 * time.Second}, err
+				}
+
+				return ctrl.Result{RequeueAfter: 5 * time.Second}, nil
+			}
+		}
+
+		masterOnLatest, err := isPodOnLatestVersion(ctx, r.Client, &master, &updatedStatefulset)
+		if err != nil {
+			log.Error(err, "could not check if pod is on latest version")
+			return ctrl.Result{RequeueAfter: 5 * time.Second}, err
+		}
+
+		// If we are here it means that all replicas
+		// are on latest version
+		if !masterOnLatest {
+			// Update Master now
+			log.Info("deleting master")
+			if err := r.Delete(ctx, &master); err != nil {
+				log.Error(err, "could not delete master")
+				return ctrl.Result{Requeue: true}, err
+			}
+			r.EventRecorder.Event(&df, corev1.EventTypeNormal, "Rollout", "Deleting master %s")
+		}
+
+		// If we are here all are on latest version
+		r.EventRecorder.Event(&df, corev1.EventTypeNormal, "Rollout", "Completed")
+
+		// update status
+		df.Status.IsRollingUpdate = false
+		if err := r.Status().Update(ctx, &df); err != nil {
+			log.Error(err, "could not update the Dragonfly object")
+			return ctrl.Result{Requeue: true}, err
+		}
+
+		return ctrl.Result{}, nil
 	} else {
 		// This is an Update
 		log.Info("updating existing resources")
@@ -107,8 +233,31 @@ func (r *DragonflyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 		}
 
 		log.Info("Updated resources for object")
-	}
 
+		// perform a rollout only if the pod spec has changed
+		var statefulSet appsv1.StatefulSet
+		if err := r.Get(ctx, client.ObjectKey{Namespace: df.Namespace, Name: df.Name}, &statefulSet); err != nil {
+			log.Error(err, "could not get statefulset")
+			return ctrl.Result{}, err
+		}
+
+		if statefulSet.Status.UpdatedReplicas != statefulSet.Status.Replicas {
+			log.Info("Pod spec has changed, performing a rollout")
+			r.EventRecorder.Event(&df, corev1.EventTypeNormal, "Rollout", "Starting a rollout")
+
+			// Start rollout and update status
+			// update status so that we can track progress
+			df.Status.IsRollingUpdate = true
+			if err := r.Status().Update(ctx, &df); err != nil {
+				log.Error(err, "could not update the Dragonfly object")
+				return ctrl.Result{Requeue: true}, err
+			}
+
+			// requeue so that the rollout is processed
+			return ctrl.Result{RequeueAfter: 5 * time.Second}, nil
+		}
+		r.EventRecorder.Event(&df, corev1.EventTypeNormal, "Resources", "Updated resources")
+	}
 	return ctrl.Result{}, nil
 }
 
