72a42a659efa5a081687783f03f331e50dda0826
Abhradeep Chakraborty <abhradeep@dragonflydb.io>
2024-01-31 18:47:05 +0530

    feat(crd): add an aclFromSecret field

--- api/v1alpha1/dragonfly_types.go
+++ api/v1alpha1/dragonfly_types.go
@@ -41,6 +41,11 @@ type DragonflySpec struct {
 	// +kubebuilder:validation:Optional
 	Args []string `json:"args,omitempty"`
 
+	// (Optional) Acl file Secret to pass to the container
+	// +optional
+	// +kubebuilder:validation:Optional
+	AclFromSecret *corev1.SecretKeySelector `json:"aclSecretRef,omitempty"`
+
 	// (Optional) Annotations to add to the Dragonfly pods.
 	// +optional
 	// +kubebuilder:validation:Optional
