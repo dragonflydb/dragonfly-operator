bc5e00d26b0d287acea0af68f2a5708838bdcefe
Sergey Shevchenko <sergeyshevchdevelop@gmail.com>
2024-07-11 07:32:57 +0300

    feat: Upgrade k8s versions (#210)
    * feat: Upgrade k8s versions
    
    Signed-off-by: Sergey Shevchenko <sergeyshevchdevelop@gmail.com>
    
    * chore: Sort imports
    
    Signed-off-by: Sergey Shevchenko <sergeyshevchdevelop@gmail.com>
    
    ---------
    
    Signed-off-by: Sergey Shevchenko <sergeyshevchdevelop@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -35,6 +35,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	dragonflydbiov1alpha1 "github.com/dragonflydb/dragonfly-operator/api/v1alpha1"
 	"github.com/dragonflydb/dragonfly-operator/internal/controller"
@@ -84,9 +86,13 @@ func main() {
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
+		Scheme: scheme,
+		Metrics: metricsserver.Options{
+			BindAddress: metricsAddr,
+		},
+		WebhookServer: webhook.NewServer(webhook.Options{
+			Port: 9443,
+		}),
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "31079dea.dragonflydb.io",
