81e729a04a447d90e20d46360161e8ab2539568d
Cyril Levis <git@levis.name>
2024-12-06 10:58:04 +0100

    fix: bind on dualstack by default
    both operator and dragonfly, should not hurt.

--- internal/resources/const.go
+++ internal/resources/const.go
@@ -99,6 +99,8 @@ const (
 var DefaultDragonflyArgs = []string{
 	"--alsologtostderr",
 	"--primary_port_http_enabled=false",
+	"--bind=::",
+	"--admin_bind=::",
 	fmt.Sprintf("--admin_port=%d", DragonflyAdminPort),
 	"--admin_nopass",
 }
