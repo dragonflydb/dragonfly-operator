65fb5c32f54004a1cd0fe5308562cf300508d44a
Abhradeep Chakraborty <abhradeep@dragonflydb.io>
2025-04-02 12:54:44 +0530

    feat(df): bump dragonfly version to v1.28.1
    Signed-off-by: Abhradeep Chakraborty <abhradeep@dragonflydb.io>

--- e2e/dragonfly_controller_test.go
+++ e2e/dragonfly_controller_test.go
@@ -305,7 +305,7 @@ var _ = Describe("Dragonfly Lifecycle tests", Ordered, FlakeAttempts(3), func() 
 			}, &df)
 			Expect(err).To(BeNil())
 
-			df.Spec.Image = fmt.Sprintf("%s:%s", resources.DragonflyImage, "v1.25.6")
+			df.Spec.Image = fmt.Sprintf("%s:%s", resources.DragonflyImage, "v1.26.2")
 			err = k8sClient.Update(ctx, &df)
 			Expect(err).To(BeNil())
 		})
