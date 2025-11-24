69a82d19999debed155e892b01fb68594f372e5c
Ihor Hrytskiv <ihor.hrytskiv@macpaw.com>
2025-11-24 13:56:01 +0200

    fix: rolling update process
    Signed-off-by: Ihor Hrytskiv <ihor.hrytskiv@macpaw.com>

--- internal/controller/dragonfly_instance.go
+++ internal/controller/dragonfly_instance.go
@@ -710,8 +710,8 @@ func (dfi *DragonflyInstance) deleteRoleLabel(ctx context.Context, pod *corev1.P
 	return nil
 }
 
-// allPodsHealthy checks whether all pods are healthy, and deletes pods that are outdated and failed to start
-func (dfi *DragonflyInstance) allPodsHealthy(ctx context.Context, updateRevision string) (ctrl.Result, error) {
+// allPodsHealthyAndHaveRole checks whether all pods are healthy, and deletes pods that are outdated and failed to start
+func (dfi *DragonflyInstance) allPodsHealthyAndHaveRole(ctx context.Context, updateRevision string) (ctrl.Result, error) {
 	pods, err := dfi.getPods(ctx)
 	if err != nil {
 		return ctrl.Result{}, fmt.Errorf("failed to get dragonfly pods: %w", err)
@@ -733,6 +733,11 @@ func (dfi *DragonflyInstance) allPodsHealthy(ctx context.Context, updateRevision
 
 		if !ready {
 			dfi.log.Info("waiting for pod to finish startup", "pod", pod.Name)
+			return ctrl.Result{RequeueAfter: 5 * time.Second}, nil
+		}
+
+		if !roleExists(&pod) {
+			dfi.log.Info("waiting for pod to be assigned a role", "pod", pod.Name)
 			return ctrl.Result{RequeueAfter: 5 * time.Second}, nil
 		}
 	}
@@ -763,10 +768,14 @@ func (dfi *DragonflyInstance) verifyUpdatedReplicas(ctx context.Context, replica
 	return ctrl.Result{}, nil
 }
 
-// updatedReplicas updates the replicas to the latest version
-func (dfi *DragonflyInstance) updatedReplicas(ctx context.Context, replicas *corev1.PodList, updateRevision string) (ctrl.Result, error) {
+// updateReplicas updates the replicas to the latest version
+func (dfi *DragonflyInstance) updateReplicas(ctx context.Context, replicas *corev1.PodList, updateRevision string) (ctrl.Result, error) {
 	for _, replica := range replicas.Items {
 		if !isPodOnLatestVersion(&replica, updateRevision) {
+			_, err := dfi.getMaster(ctx)
+			if err != nil {
+				return ctrl.Result{}, fmt.Errorf("skipping deleting replica: failed to get master: %w", err)
+			}
 			dfi.log.Info("deleting replica", "pod", replica.Name)
 			dfi.eventRecorder.Event(dfi.df, corev1.EventTypeNormal, "Rollout", "Deleting replica")
 			if err := dfi.client.Delete(ctx, &replica); err != nil {
